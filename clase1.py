3919ece75f43697dc9696a3dc37a93922fc2f57d
Addrriel <adriel.programacion@hotmail.com>
2026-01-04 00:32:00 -0500

    Clase 1 de python: Comentarios, tipo de datos, buscar los tipos, operaciones básicas

---
+++ clase1.py
@@ -0,0 +1,96 @@
+# 1. Imprime "¡Hola Mundo!" por consola.
+
+# 2. Escribe un comentario de una sola lí­nea explicando quÃ© hace el código del Ejercicio 1.
+
+# 3. Imprime tu nombre y edad en la misma línea utilizando la funciÃ³n print.
+
+# 4. Usa la función type() para imprimir el tipo de dato de una cadena de texto, un número entero y un número decimal.
+
+# 5. Escribe un comentario en varias líneas explicando quÃ© son los tipos de datos en Python.
+
+# 6. Imprime el resultado de concatenar dos cadenas de texto, por ejemplo: "Hola" y "Mundo".
+
+# 7. Crea una variable para almacenar tu nombre, otra para tu edad, e imprime ambas en la misma línea.
+
+# 8. Escribe un programa que solicite al usuario su nombre y lo imprima junto con un saludo.
+
+# 9. Usa print() para mostrar el resultado de la suma de dos números enteros y el tipo de dato resultante.
+
+# 10. Comenta el cÃ³digo del Ejercicio 9, y explica quÃ© hace cada línea usando comentarios de una sola línea.
+
+"""
+Ejercicio 1
+# 1. Imprime "¡Hola Mundo!" por consola.
+"""
+print("¡Hola Mundo!")
+
+"""
+Ejercicio 2
+# 2. Escribe un comentario de una sola lí­nea explicando quÃ© hace el código del Ejercicio 1.
+"""
+#Básicamente hace un mensaje a la consola del IDE donde mostrará el mensaje que tiene dentro del hola mundo
+
+"""
+Ejercicio 3
+# 3. Imprime tu nombre y edad en la misma línea utilizando la funciÃ³n print.
+"""
+print('Mi nombre es Alejandro Bedoya y tengo 23 años')
+
+"""
+Ejercicio 4
+4. Usa la función type() para imprimir el tipo de dato de una cadena de texto, un número entero y un número decimal.
+"""
+print(type("hola, soy un texto"))
+print(type(4))
+print(type(3.14))
+
+"""
+Ejercicio 5
+
+5. Escribe un comentario en varias líneas explicando quÃ© son los tipos de datos en Python.
+
+Los tipos de dato de python son datos que se relacionan a ejemplos del mundo real, una cadena de texto para decir palabras, int para poder
+escribir números, floats para hacer cálculos de moneda o alguna particularidad que no sea exacta, boolean sirve para condicionales y ver si
+algo es verdadero o falso, etc
+
+"""
+
+"""
+Ejercicio 6
+
+6. Imprime el resultado de concatenar dos cadenas de texto, por ejemplo: "Hola" y "Mundo".
+
+"""
+print("Hola", "Mundo")
+
+"""
+Ejercicio 7
+
+7. Crea una variable para almacenar tu nombre, otra para tu edad, e imprime ambas en la misma línea.
+
+"""
+nombreCompleto = "Alejandro Bedoya"
+edad = 23
+print("Mi nombre es" , nombreCompleto , "y tengo la edad de" , edad , "años.")
+
+"""
+Ejercicio 8
+
+8. Escribe un programa que solicite al usuario su nombre y lo imprima junto con un saludo.
+
+"""
+nombreUsuario = input("Escriba su nombre completo: ")
+print("Saludos, compañero", nombreUsuario, ".Eres bienvenido a mi primer programa de python")
+
+
+"""
+Ejercicio 9
+9. Usa print() para mostrar el resultado de la suma de dos números enteros y el tipo de dato resultante.
+"""
+print(4+2, type(4+2))
+
+"""
+Ejercicio 10
+10. Comenta el cÃ³digo del Ejercicio 9, y explica quÃ© hace cada línea usando comentarios de una sola línea.
+"""
+#Use un print para poder poner 2 valores: la suma y el tipo; al colocar la suma pongo la "," para poder separar las operaciones y me salgan ambos
